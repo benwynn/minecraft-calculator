cffbbe0c366a2df2e21827a23f77b951ce6dbcd4
angelawynn <angie@glowie.com>
2023-07-01 15:54:02 -0400

    Getting basic interface in place

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ import Calculator from "./calculator.js"
 
 var cookieNotice      = document.querySelector(".cookie-notice");
 var cookieNoticeClose = document.querySelector(".cookie-notice-dismiss-button");
+var recipeInput = document.querySelector(".RecipeInput");
+var goButton = document.querySelector(".GoButton");
+var outputBlock = document.querySelector(".output")
+
 
 cookieNoticeClose.addEventListener("click", function() {
   cookieNotice.parentNode.removeChild(cookieNotice);
@@ -11,6 +15,22 @@ cookieNoticeClose.addEventListener("click", function() {
 
 var calc = new Calculator(recipes);
 
+goButton.addEventListener("click", callCalculate);
+
+recipeInput.addEventListener("keydown", function(e) {
+  if(e.code == "Enter") {
+    callCalculate();
+  }
+});
+
+function callCalculate()
+{
+  let value=recipeInput.value;
+  console.log(value);
+  if (!value) return;
+  let output=calc.calculate(value);
+  outputBlock.innerHTML = JSON.stringify(output);
+};
 
 console.log("Starting Recipes:");
 console.log(JSON.stringify(recipes));
