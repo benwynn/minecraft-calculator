17e594d05ae1d831a278da5bf9d7af57447875ff
angelawynn <angie@glowie.com>
2023-07-02 00:40:00 -0400

    setting up accumulator

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,9 @@
 
 
 class Calculator {
+    accumulator = {};
+    remainder = {};
+
     constructor(recipeDB) {
         this.recipeDB = recipeDB;
     }
@@ -13,10 +16,20 @@ class Calculator {
         if(result.quantity > 1) result.name +="s";
         title.innerHTML = `${result.quantity} ${result.name}`;
         output.appendChild(title);
-        result.mats.forEach(obj => {
-            let variableName = this.outputMats(obj);
-            output.appendChild(variableName);
-        })
+        // result.mats.forEach(obj => {
+        //     let variableName = this.outputMats(obj);
+        //     output.appendChild(variableName);
+        // })
+        let items = Object.keys(this.accumulator);
+        items.forEach( itemName => {
+            let text = document.createElement("div");
+            let name = itemName;
+        if(this.accumulator[itemName].quantity > 1) {
+            name += "s";
+        }
+            text.innerHTML =`${name} ${this.accumulator[itemName].quantity}`
+            output.appendChild(text);
+        });
         return output;
     }
 
@@ -55,26 +68,43 @@ class Calculator {
         }
 
         result.quantity = 0;
+        
+        result.quantity += recipe.getQuantity();
         result.mats = []
 
-        while (result.quantity < quantity) {
-            result.quantity += recipe.getQuantity();
+        while (quantity > 0) { 
+            this.accumulate(target,quantity,recipe.getQuantity());
             recipe.getMats().forEach(obj =>{
                 let subresult = this.getResult(obj.name,obj.quantity);
                 console.log(subresult);
-                // if(!subresult.mats) {
-                //     result.mats.push(subresult);
-                // } else {
-                // subresult.mats.forEach(subobj =>{
-                //     result.mats.push(subobj);
-                // })}
-                if(subresult) result.mats.push(subresult);
-            })
+                if(subresult) {
+                    result.mats.push(subresult);
+                }
+                quantity -= recipe.getQuantity();
+            });
+            result.quantity += recipe.getQuantity();
         }
+        
         result.name = target;
 
         return result;
     }
+
+    accumulate(item,quantity,produced) {
+        if(!this.accumulator.hasOwnProperty(item)) {
+            this.accumulator[item] = {quantity:0,remainder:0};
+        }
+        if(quantity < this.accumulator[item].remainder) {
+            this.accumulator[item].remainder -= quantity;
+            this.accumulator[item].quantity += quantity;
+            return false;
+        } 
+        this.accumulator[item].quantity += quantity;
+        this.accumulator[item].remainder += (produced - quantity);
+        return true;
+    }
+        
 }
+
 
 export default Calculator;
