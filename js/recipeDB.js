6d393008f2dba4aa83795268594d999374795406
Ben Wynn <bwynn@glowie.com>
2023-07-01 20:05:00 -0400

    adding recipe and recipedb objects

---
+++ js/recipeDB.js
@@ -0,0 +1,24 @@
+import Recipe from "./recipe.js"
+
+class RecipeDB {
+
+    #recipeMap = {};
+
+    constructor(recipes) {
+        this.recipes = recipes;
+        Object.keys(this.recipes).forEach(recipeName => {
+            let recipeData = recipes[recipeName];
+            let recipe = new Recipe(recipeName, recipeData.level, recipeData.quantity, recipeData.mats);
+            this.#recipeMap[recipe.getName()] = recipe;
+        })
+    }
+
+    getRecipe(target) {
+        if (this.#recipeMap.hasOwnProperty(target)) {
+            return this.#recipeMap[target];
+        }
+        return null;
+    }
+}
+
+export default RecipeDB
