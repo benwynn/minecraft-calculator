024ee0820064809da5f2f9989f656616dd168a6a
Ben Wynn <bwynn@glowie.com>
2023-07-04 14:40:26 -0400

    indentation and formatting

--- js/calculator.js
+++ js/calculator.js
@@ -38,17 +38,17 @@ class Calculator {
             let name = itemName;
             let suffix = "";
 
-            if(this.addSuffix(name,this.accumulator[itemName].quantity)) {
+            if (this.addSuffix(name,this.accumulator[itemName].quantity)) {
                 suffix += "s";
             }
             let level = this.recipeDB.getRecipe(itemName).getLevel();
             text.innerHTML =`${this.accumulator[itemName].quantity} ${name}${suffix} Level ${level} `;
             output.appendChild(text);
             suffix = "";
-            if(this.addSuffix(name,this.accumulator[itemName].remainder)) {
+            if (this.addSuffix(name,this.accumulator[itemName].remainder)) {
                 suffix += "s";
             }
-            if( this.accumulator[itemName].remainder > 0) {
+            if (this.accumulator[itemName].remainder > 0) {
                 remainderText.innerHTML = `${this.accumulator[itemName].remainder} ${name}${suffix}`;
                 remainders.appendChild(remainderText);
             }
@@ -57,25 +57,25 @@ class Calculator {
         return output;
     }
 
-reset() {
-    this.accumulator = [];
-    this.remainder = [];
-}
+    reset() {
+        this.accumulator = [];
+        this.remainder = [];
+    }
 
-addSuffix(text,quantity){
-    if(quantity < 2) return false;
-    if(text.slice(-1)=="s") return false;
-    return true;
-}
+    addSuffix(text,quantity) {
+        if(quantity < 2) return false;
+        if(text.slice(-1)=="s") return false;
+        return true;
+    }
 
-    outputMats (mat){
+    outputMats (mat) {
         let block = document.createElement("div");
         block.className = "output-row";
-        if(mat.quantity > 1) mat.name+= "s";
+        if (mat.quantity > 1) mat.name+= "s";
         let text = document.createElement("div");
         text.innerHTML = `${mat.quantity} ${mat.name}`;
         block.appendChild(text);
-        if(mat.mats) mat.mats.forEach(obj => {
+        if (mat.mats) mat.mats.forEach(obj => {
             let result = this.outputMats(obj);
             block.appendChild(result);
         });
@@ -95,7 +95,7 @@ addSuffix(text,quantity){
         }
 
         if (!recipe.getMats().length) {
-            if(recipe.getQuantity()) {
+            if (recipe.getQuantity()) {
                 this.accumulate(target,quantity,recipe.getQuantity());
             } else {
                 this.accumulate(target,quantity,1);
@@ -111,7 +111,7 @@ addSuffix(text,quantity){
             this.accumulate(target,quantity,recipe.getQuantity());
             recipe.getMats().forEach(obj =>{
                 let subresult = this.getResult(obj.name,obj.quantity);
-                if(subresult) {
+                if (subresult) {
                     result.mats.push(subresult);
                 }
                 quantity -= recipe.getQuantity();
@@ -125,15 +125,15 @@ addSuffix(text,quantity){
     }
 
     accumulate(item,quantity,produced) {
-        if(!this.accumulator.hasOwnProperty(item)) {
+        if (!this.accumulator.hasOwnProperty(item)) {
             this.accumulator[item] = {quantity:0,remainder:0};
         }
-        if(quantity < this.accumulator[item].remainder) {
+        if (quantity < this.accumulator[item].remainder) {
             this.accumulator[item].remainder -= quantity;
             this.accumulator[item].quantity += quantity;
             return false;
         } 
-        if(produced > quantity) {
+        if (produced > quantity) {
             this.accumulator[item].quantity += quantity;
             this.accumulator[item].remainder += (produced - quantity);
             return true;
